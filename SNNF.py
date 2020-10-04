7a289308d0164874c1b28b8c34693a19deb0d4cb
Marco Baturan <marco.baturan@gmail.com>
2020-10-05 01:02:39 +0200

    Update SNNF.py

--- SNNF.py
+++ SNNF.py
@@ -11,7 +11,7 @@
     
     Se puede lograr ejecutar un conjunto de operaciones mentales de
     tipo simbólico. Éste programa es una representación abstracta
-    de bjao nivel de talles. Pero representa los estados internos
+    de bajo nivel de talles. Pero representa los estados internos
     de un nemonista a nivel general.
     ---[EN]---
         Computer simulation of the mental operations performed by
